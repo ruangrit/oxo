e69af128d55f8d095053bc5a4d7d54793ff2e31d
Nattapol Kengkuntod <nattapol.ke@gmail.com>
2016-06-14 21:59:23 +0700

    image slide modal

--- sites/all/themes/parallax_zymphonies_theme/templates/page--node--1.tpl.php
+++ sites/all/themes/parallax_zymphonies_theme/templates/page--node--1.tpl.php
@@ -439,12 +439,10 @@
 <div class="modal fade" id="roomsModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><img src="/sites/all/themes/parallax_zymphonies_theme/images/close.svg" /></span></button>
-        <h4 class="modal-title text-modal" id="myModalLabel">Rooms</h4>
-      </div>
       <div class="modal-body slide-modal">
-
+		
+        <h4 class="modal-title text-modal" id="myModalLabel">Rooms</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><img src="/sites/all/themes/parallax_zymphonies_theme/images/close.svg" /></span></button>
 		      		<div class="group-slide">
 
 	      				<?php 
